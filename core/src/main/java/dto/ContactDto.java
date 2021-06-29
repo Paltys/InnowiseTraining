0e12a33af4fccf1cb25d3a0757443578fdb7ca5d
Alexandr Krivosheyeu <Paltys01@yandex.ru>
2021-06-29 18:19:15 +0300

    getAll done

--- core/src/main/java/dto/ContactDto.java
+++ core/src/main/java/dto/ContactDto.java
@@ -1,8 +1,13 @@
 package dto;
 
 import entity.ContactEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ContactDto {
 
     private int id;
@@ -41,156 +46,4 @@ public class ContactDto {
         index = contact.getIndex();
     }
 
-    public ContactDto() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getMiddleName() {
-        return middleName;
-    }
-
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
-
-    public String getDataBirthday() {
-        return dataBirthday;
-    }
-
-    public void setDataBirthday(String dataBirthday) {
-        this.dataBirthday = dataBirthday;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getCitizenship() {
-        return citizenship;
-    }
-
-    public void setCitizenship(String citizenship) {
-        this.citizenship = citizenship;
-    }
-
-    public String getMaritalStatus() {
-        return maritalStatus;
-    }
-
-    public void setMaritalStatus(String maritalStatus) {
-        this.maritalStatus = maritalStatus;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
-
-    public void setWebsite(String website) {
-        this.website = website;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getWorkplace() {
-        return workplace;
-    }
-
-    public void setWorkplace(String workplace) {
-        this.workplace = workplace;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getTown() {
-        return town;
-    }
-
-    public void setTown(String town) {
-        this.town = town;
-    }
-
-    public String getHouse() {
-        return house;
-    }
-
-    public void setHouse(String house) {
-        this.house = house;
-    }
-
-    public String getFlat() {
-        return flat;
-    }
-
-    public void setFlat(String flat) {
-        this.flat = flat;
-    }
-
-    public int getIndex() {
-        return index;
-    }
-
-    public void setIndex(int index) {
-        this.index = index;
-    }
-
-    @Override
-    public String toString() {
-        return "ContactResponseDto{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", middleName='" + middleName + '\'' +
-                ", dataBirthday=" + dataBirthday +
-                ", gender='" + gender + '\'' +
-                ", citizenship='" + citizenship + '\'' +
-                ", maritalStatus='" + maritalStatus + '\'' +
-                ", website='" + website + '\'' +
-                ", email='" + email + '\'' +
-                ", workplace='" + workplace + '\'' +
-                ", country='" + country + '\'' +
-                ", town='" + town + '\'' +
-                ", house='" + house + '\'' +
-                ", flat='" + flat + '\'' +
-                ", index=" + index +
-                '}';
-    }
 }
--- core/src/main/java/entity/ContactEntity.java
+++ core/src/main/java/entity/ContactEntity.java
@@ -1,5 +1,11 @@
 package entity;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -14,6 +20,11 @@ import java.util.Objects;
 
 @Entity
 @Table(name = "contact")
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@Accessors(chain = true)
 public class ContactEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -70,179 +81,4 @@ public class ContactEntity {
     @Column(name = "index")
     private int index;
 
-
-    public ContactEntity() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getMiddleName() {
-        return middleName;
-    }
-
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
-
-    public Instant getDataBirthday() {
-        return dataBirthday;
-    }
-
-    public void setDataBirthday(Instant dataBirthday) {
-        this.dataBirthday = dataBirthday;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    public String getCitizenship() {
-        return citizenship;
-    }
-
-    public void setCitizenship(String citizenship) {
-        this.citizenship = citizenship;
-    }
-
-    public Maritalstatus getMaritalStatus() {
-        return maritalStatus;
-    }
-
-    public void setMaritalStatus(Maritalstatus maritalStatus) {
-        this.maritalStatus = maritalStatus;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
-
-    public void setWebsite(String website) {
-        this.website = website;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getWorkplace() {
-        return workplace;
-    }
-
-    public void setWorkplace(String workplace) {
-        this.workplace = workplace;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getTown() {
-        return town;
-    }
-
-    public void setTown(String town) {
-        this.town = town;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getHouse() {
-        return house;
-    }
-
-    public void setHouse(String house) {
-        this.house = house;
-    }
-
-    public String getFlat() {
-        return flat;
-    }
-
-    public void setFlat(String flat) {
-        this.flat = flat;
-    }
-
-    public int getIndex() {
-        return index;
-    }
-
-    public void setIndex(int index) {
-        this.index = index;
-    }
-
-    @Override
-    public String toString() {
-        return "ContactEntity{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", middleName='" + middleName + '\'' +
-                ", dataBirthday=" + dataBirthday +
-                ", gender=" + gender +
-                ", citizenship='" + citizenship + '\'' +
-                ", maritalStatus=" + maritalStatus +
-                ", website='" + website + '\'' +
-                ", email='" + email + '\'' +
-                ", workplace='" + workplace + '\'' +
-                ", country='" + country + '\'' +
-                ", town='" + town + '\'' +
-                ", street='" + street + '\'' +
-                ", house='" + house + '\'' +
-                ", flat='" + flat + '\'' +
-                ", index=" + index +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ContactEntity that = (ContactEntity) o;
-        return id == that.id && index == that.index && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(middleName, that.middleName) && Objects.equals(dataBirthday, that.dataBirthday) && gender == that.gender && Objects.equals(citizenship, that.citizenship) && maritalStatus == that.maritalStatus && Objects.equals(website, that.website) && Objects.equals(email, that.email) && Objects.equals(workplace, that.workplace) && Objects.equals(country, that.country) && Objects.equals(town, that.town) && Objects.equals(street, that.street) && Objects.equals(house, that.house) && Objects.equals(flat, that.flat);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, firstName, lastName, middleName, dataBirthday, gender, citizenship, maritalStatus, website, email, workplace, country, town, street, house, flat, index);
-    }
 }
